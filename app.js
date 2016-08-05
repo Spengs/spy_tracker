a873ae99b02ae9ace92d24f9032ebfd0d1da0253
Spengs <Jotto465@gmail.com>
2016-08-04 20:50:20 -0500

    delete button works, turns red

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
 $(document).ready(function(){
 console.log("working")
 $(".addspy").on("click", newSpy);
-$(".deleteSpy").on("click", removeSpy);//i just realized that this is referring to elements not yet on the DOM,
-// and that I should have used body there, or the empty container i created with the intent of using for all
-// these spies
+$("body").on("click", removeSpy);
+$("body").on("click", spyStatusRed);
+// $("body").on("click", spyStatusYellow);
 });
 
 var spyArray = [];
 
 
-
 function newSpy(){
   $('body').append("<div class ='spy' style='background-color: yellow'>" + (spyArray.length += 1) + "<button class='status'>Status</button><button class='deletespy'>Delete</button></div>");
-  $('body').children().last().data('id', spyArray[i]);
+  $('body').children().last();
 };
 
-// function spyStatus(){
+function spyStatusRed(){
+  $(".status").on("click", function(){
+    if($(this).parent().css("background-color","yellow")){
+      $(this).parent().css("background-color","red");
+    }
+});
+}
+// function spyStatusYellow(){
 //   $(".status").on("click", function(){
-//     if($(this).css("background-color","yellow"){
-//       $(this).css("background-color","red");
-//     }else{
-//       $(this).css("background-color","yellow");
+//     if($(this).parent().css("background-color","red")){
+//       $(this).parent().css("background-color","yellow");
 //     }
-//     }
+//   });
 //   }
 
 function removeSpy(){
   $(".deletespy").on("click", function(){
-    console.log("do i work?");
+    $(this).parent().remove();
   });
-
-  // $(this).remove();
 }
